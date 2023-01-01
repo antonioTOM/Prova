c22a0f69cf20399a02f3ab692331dcdc6c8c4ea5
antonioTOM <110413106+antonioTOM@users.noreply.github.com>
2023-01-01 17:16:43 +0100

    Update task5.js
    Update task5.js, worked on long string composition; codespace not working => code needs to be tested before starting new parts.

--- Code-Tasks/task5.js
+++ Code-Tasks/task5.js
@@ -9,7 +9,12 @@ function ord(char) {
 }
 
 function main(t, l, words, s1, s2, n, a, b, c, d) {
-    
+    const longStringChars = [s1, s2];
+    for (let charNr = longStringChars.length; charNr < 50; charNr++) {
+        let xi = (a * ord(longStringChars[charNr - 1] + b * ord(longStringChars[charNr - 2]) + c) % d;
+        let si = char(97 + (xi % 26));
+    }
+    const longString = longStringChars.join('');
 }
 
 main(1, 5, ['axpaj', 'apxaj', 'dnrbt', 'pjxdn', 'abd'], 'a', 'a', 50, 1, 1, 1, 30);
